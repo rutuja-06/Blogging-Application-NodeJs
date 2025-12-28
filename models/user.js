453de2feafbf0f0ff59734b65fc0f195ea6aaf1d
Rutuja Jogdand <rutujajogdand06@gmail.com>
2025-12-28 14:26:13 +0530

    signup form started

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const { createHmac, randomBytes } = require('node:crypto');
+const {Schema , model} = require('mongoose');
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    profileImageURL:{
+        type: String,
+        default: "/images/userimage.jpg"
+    },
+    role:{
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+},
+{timestamps: true}
+);
+
+userSchema.pre("save", function (next){
+    const user = this;
+
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt).update(user.password).digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+});
+
+const User = model("user", userSchema);
+
+module.exports= User; 
