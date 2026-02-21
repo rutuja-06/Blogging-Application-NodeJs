bb760899cdb32e7abda3bc81e67a43b3ef0948cf
Rutuja Jogdand <rutujajogdand06@gmail.com>
2026-02-22 03:26:41 +0530

    create a blog page save file in public folder and use it again to show

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,14 +4,14 @@ function checkForAuthenticationCookie(cookieName){
     return (req, res, next) => {
         const tokenCookieValue = req.cookies[cookieName];
         if(!tokenCookieValue){
-            next();
+           return next();
         }
 
         try {
             const userPayload = validateToken(tokenCookieValue);
             req.user = userPayload;
         } catch (error) {}
-        next();
+        return next();
     }
 }
 
