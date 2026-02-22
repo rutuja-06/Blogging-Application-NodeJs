12fe857d9157f158bb425ea72b4f14501ca60011
Rutuja Jogdand <rutujajogdand06@gmail.com>
2026-02-23 01:30:23 +0530

    populate, created multi level pages, used conditions

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,10 @@
 const {Router} = require("express");
 const multer = require("multer");
 const path = require("path");
+
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
+
 const router = Router();
 
 const storage = multer.diskStorage({
@@ -20,6 +23,28 @@ router.get('/add-new', (req, res) =>{
     return res.render("addBlog", {
         user: req.user,
     });
+});
+
+router.get('/:id', async (req, res) =>{
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  console.log("comments", comments);
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  })
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  const comment = await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post('/', upload.single('coverImage'), async (req, res) =>{
